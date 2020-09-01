777a66cef26ba75501e9e8f809dafd75bf8d97ca
Seth Townsend <jstownsend1@crimson.ua.edu>
2020-09-01 12:01:22 -0500

    WIP: Move Message creation into respective message structures 2

---
+++ networking/src/messages/get_blocks.rs
@@ -0,0 +1,63 @@
+use shared::{CompactInt,Bytes, u256};
+use config::Config;
+use log::warn;
+
+pub struct GetBlocks{
+    protocol_version: u32,
+    hash_count: CompactInt,
+    block_header_hashes: Vec<u256>,
+    stop_hash: u256
+}
+
+impl GetBlocks{
+    pub fn new(block_hashes: Vec<u256>,
+        inv_message: bool,
+        config: &Config) -> GetBlocks{
+        let mut message = GetBlocks {
+            protocol_version: config.get_protocol_version(),
+            hash_count: CompactInt::from(block_hashes.len()),
+            block_header_hashes: block_hashes,
+            stop_hash: u256::new(),
+        };
+        if !inv_message{
+            //The header hash of the last header hash being requested; set to all zeroes to request an “inv” message
+            //with all subsequent header hashes (a maximum of 500 will be sent as a reply to this message; 
+            //if you need more than 500, you will need to send another “getblocks” message with a higher-height 
+            //header hash as the first entry in block header hash field).
+            match block_hashes.last() {
+                Some(hash) => message.stop_hash = *hash.clone(),
+                None => {
+                    warn!("GetBlocks: stop hash was empty");
+                }
+            }
+        }
+        message
+    }
+
+    // pub fn new(payload: Payload::GetBlocksPayload,config: &Config) -> GetBlocks {
+    //     let mut message = GetBlocks {
+    //         protocol_version: config.get_protocol_version(),
+    //         hash_count: CompactInt::from(payload.block_hashes.len()),
+    //         block_header_hashes: Vec.new(),
+    //         stop_hash: Vec.new(),
+    //     };
+    //     for hash in payload.block_hashes.iter() {
+    //         message.block_header_hashes.append(hash)
+    //     }
+    //     if *inv_message {
+    //         message.body.append(u256::new());
+    //     } else {
+    //         match payload.block_hashes.last() {
+    //             Some(hash) => message.body.append(hash),
+    //             None => {
+    //                 warn!("GetBlocks: stop hash was empty");
+    //                 message.body.append(u256::new());
+    //             }
+    //         }
+    //     }
+    //     //msg.create_header_for_body(Command::GetBlocks, config.magic());
+    //     return message;    
+    //}
+
+}
+
---
+++ networking/src/messages/get_data.rs
@@ -0,0 +1,52 @@
+use shared::{CompactInt,Serializable, u256};
+use config::Config;
+pub struct GetData{
+    hash_count: CompactInt,
+    inventory: Vec<InventoryData>,
+}
+#[derive(Copy, Clone)]
+pub enum InventoryType {
+    Tx = 1,
+    Block = 2,
+    FilteredBlock = 3,
+    CompactBlock = 4,
+    WitnessTx = 5,
+    WitnessBlock = 6,
+    FilteredWitnessBlock = 7,
+}
+impl Serializable for InventoryType {
+    fn serialize(&self, target: &mut Vec<u8>) {
+        (*self as u32).serialize(target)
+    }
+}
+pub struct InventoryData {
+    pub inventory_type: InventoryType,
+    pub hash: u256,
+}
+impl InventoryData{
+    pub fn from(inv: InventoryType, hash: u256) -> InventoryData{
+        InventoryData {
+            inventory_type: inv,
+            hash: hash
+        }
+    }
+}
+
+impl GetData{
+    pub fn new(temp_inventory: Vec<InventoryData>, config: &Config) -> GetData {
+        let mut message = GetData{
+            hash_count: CompactInt::from(temp_inventory.len()),
+            inventory: temp_inventory,
+        };
+        // for x in inventory.iter() {
+        //     message.inventory.append(
+        //         InventoryData{
+        //             x.inventory_type,
+        //             x.hash,
+        //         }
+        //     );
+        // }
+        //message.create_header_for_body(Command::GetData, config.magic());
+        message
+    }
+}
