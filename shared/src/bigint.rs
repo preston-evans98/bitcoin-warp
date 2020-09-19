9db6a6b2216f62ed64d9a9573325e84cdc37fb63
Preston Evans <pbevans1@crimson.ua.edu>
2020-09-19 09:59:27 -0500

    Add hex, from_be to u256. Test.

--- shared/src/bigint.rs
+++ shared/src/bigint.rs
@@ -1,6 +1,6 @@
 use crate::deserializable::{Deserializable, DeserializationError};
 use crate::serializable::Serializable;
-use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use byteorder::{BigEndian, LittleEndian, ReadBytesExt, WriteBytesExt};
 
 #[allow(non_camel_case_types)]
 #[derive(Debug)]
@@ -22,6 +22,24 @@ impl u256 {
         u256([target, 0, 0, 0])
     }
 
+    pub fn from_bytes_be(target: Vec<u8>) -> Result<u256, DeserializationError> {
+        let mut cursor = std::io::Cursor::new(target);
+        u256::deserialize_be(&mut cursor)
+    }
+
+    // A big_endian counterpart to deser
+    pub fn deserialize_be<R>(target: &mut R) -> Result<u256, DeserializationError>
+    where
+        R: std::io::Read,
+    {
+        let fourth = target.read_u64::<BigEndian>()?;
+        let third = target.read_u64::<BigEndian>()?;
+        let second = target.read_u64::<BigEndian>()?;
+        let first = target.read_u64::<BigEndian>()?;
+        Ok(u256([first, second, third, fourth]))
+    }
+
+    // Read from big_endian hex
     pub fn from_hex(target: &str) -> u256 {
         // TODO: Test
         let mut result = u256([0, 0, 0, 0]);
@@ -46,6 +64,13 @@ impl u256 {
             }
         }
         result
+    }
+    // writes contents to big_endian hex
+    pub fn to_hex(&self) -> String {
+        format!(
+            "{:x}{:x}{:x}{:x}",
+            self.0[3], self.0[2], self.0[1], self.0[0]
+        )
     }
 }
 
@@ -97,9 +122,22 @@ impl PartialEq for u256 {
 fn test_u256_ser_deser() {
     let expected = u256([1, 2, 3, 4]);
     let mut serial: Vec<u8> = Vec::with_capacity(32);
-    // let mut ser_cursor = std::io::Cursor::new(&serial);
     expected.serialize(&mut serial).unwrap();
     let mut de_cursor = std::io::Cursor::new(serial);
     let actual = u256::deserialize(&mut de_cursor).unwrap();
     assert_eq!(expected, actual);
+}
+
+#[test]
+fn test_u256_hex() {
+    use warp_crypto::double_sha256;
+
+    let from_hex =
+        u256::from_hex("9595c9df90075148eb06860365df33584b75bff782a510c6cd4883a419833d50");
+    let from_bytes = u256::from_bytes_be(double_sha256(&b"hello".to_vec())).unwrap();
+    assert_eq!(from_bytes, from_hex);
+    assert_eq!(
+        &from_hex.to_hex(),
+        "9595c9df90075148eb06860365df33584b75bff782a510c6cd4883a419833d50"
+    )
 }
