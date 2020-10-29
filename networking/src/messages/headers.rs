b1d6a132ad4105c3e9a36ed5b5358d5042144c29
Preston Evans <pbevans1@crimson.ua.edu>
2020-10-29 11:18:54 -0500

    impl Ser/De for Headers

--- networking/src/messages/headers.rs
+++ networking/src/messages/headers.rs
@@ -1,15 +1,42 @@
 use crate::block_header::BlockHeader;
-use serde_derive::{Deserializable, Serializable};
-use shared::CompactInt;
-use shared::Serializable;
-#[derive(Deserializable, Serializable)]
+use shared::{CompactInt, Deserializable, DeserializationError, Serializable};
 pub struct Headers {
     headers: Vec<BlockHeader>,
 }
 
+impl Serializable for Headers {
+    fn serialize<W>(&self, target: &mut W) -> Result<(), std::io::Error>
+    where
+        W: std::io::Write,
+    {
+        CompactInt::from(self.headers.len()).serialize(target)?;
+        for item in self.headers.iter() {
+            item.serialize(target)?;
+            0u8.serialize(target)?;
+        }
+        Ok(())
+    }
+}
+
+impl Deserializable for Headers {
+    fn deserialize<R>(reader: &mut R) -> Result<Headers, DeserializationError>
+    where
+        R: std::io::Read,
+    {
+        let count = CompactInt::deserialize(reader)?;
+        let mut result = Vec::with_capacity(count.value() as usize);
+        for _ in 0..result.len() {
+            result.push(BlockHeader::deserialize(reader)?);
+            let _ = u8::deserialize(reader)?;
+        }
+        Ok(Headers { headers: result })
+    }
+}
+
 impl crate::payload::Payload for Headers {
     fn to_bytes(&self) -> Result<Vec<u8>, std::io::Error> {
-        let len = self.headers.len() * BlockHeader::len() + CompactInt::size(self.headers.len());
+        let len =
+            self.headers.len() * (BlockHeader::len() + 1) + CompactInt::size(self.headers.len());
         let mut result = Vec::with_capacity(len);
         self.serialize(&mut result)?;
         Ok(result)
