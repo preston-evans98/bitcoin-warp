7c6cdf9a6e36eaae4b9c65db5ec8590e7db25f7f
Preston Evans <pbevans1@crimson.ua.edu>
2020-12-01 08:09:03 -0800

    Update tokio version

--- networking/src/messages/merkle_block.rs
+++ networking/src/messages/merkle_block.rs
@@ -2,18 +2,23 @@ use crate::block_header::BlockHeader;
 use serde_derive::{Deserializable, Serializable};
 use shared::{CompactInt, Serializable};
 #[derive(Deserializable, Serializable)]
-pub struct MerkleBlock{
+pub struct MerkleBlock {
     block_header: BlockHeader,
     transaction_count: u32,
     //hashCount
-    hashes: Vec<u256>, 
+    hashes: Vec<u256>,
     //flagByteCount
-    flags: Vec<Bytes>
+    flags: Vec<u8>,
 }
-impl crate::payload::Payload{
-    fn to_bytes(&self) -> Result<Vec<u8>, std::io::Error>{
+impl crate::payload::Payload {
+    fn to_bytes(&self) -> Result<Vec<u8>, std::io::Error> {
         let mut size = 0;
-        size += BlockHeader::len() + 4 + CompactInt::size(self.hashes.len()) + (self.hashes.len() * 32) + CompactInt::size(self.flags.len()) + self.flags.len(); //32 bytes for each "hash" as they are u256
+        size += BlockHeader::len()
+            + 4
+            + CompactInt::size(self.hashes.len())
+            + (self.hashes.len() * 32) //32 bytes for each "hash" as they are u256
+            + CompactInt::size(self.flags.len())
+            + self.flags.len();
         let mut target = Vec::with_capacity(size);
         self.serialize(&mut target)?;
         Ok(target)
